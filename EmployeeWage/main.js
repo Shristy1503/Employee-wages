aaf2e22e104f4df1ad000fe03bdd2b4c0248cff2
Shristy1503 <shriimishra15@gmail.com>
2025-03-04 16:39:53 +0530

    [Shristy] Add.UC2 Functionality

--- EmployeeWage/main.js
+++ EmployeeWage/main.js
@@ -1,9 +1,24 @@
-const IS_ABSENT = 0;
+const WAGE_PER_HOUR = 20;
+const FULL_TIME_HOURS = 8;
+const PART_TIME_HOURS = 4;
 
-let attendance = Math.floor(Math.random() * 10) % 2;
-if(attendance == IS_ABSENT){
-    console.log("Employee is absent");
-    return;
-}else {
-    console.log("Employee is present");
+let work = Math.floor(Math.random() * 3);
+let workHours = 0;
+
+switch (work) {
+    case 1:
+        workHours = PART_TIME_HOURS;
+        console.log("Employee worked Part Time");
+        break;
+    case 2:
+        workHours = FULL_TIME_HOURS;
+        console.log("Employee worked Full Time");
+        break;
+    default:
+        workHours = 0;
+        console.log("Employee did not work");
+        break;
 }
+
+let dailyWage = workHours * WAGE_PER_HOUR;
+console.log("Daily Wage: $" + dailyWage);
