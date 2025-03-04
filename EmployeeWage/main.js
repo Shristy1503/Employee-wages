4e5ad61f9ef7f718007f5410144952d051d82c8d
Shristy1503 <shriimishra15@gmail.com>
2025-03-04 16:56:50 +0530

    [Shristy] Add.UC3 Functionality

--- EmployeeWage/main.js
+++ EmployeeWage/main.js
@@ -2,23 +2,25 @@ const WAGE_PER_HOUR = 20;
 const FULL_TIME_HOURS = 8;
 const PART_TIME_HOURS = 4;
 
+function getWorkHours(work){
+    let workHours;
+    switch(work){
+        case 1:
+            workHours = PART_TIME_HOURS;
+            console.log("Employee worked Part Time");
+            break;
+        case 2:
+            workHours = FULL_TIME_HOURS;
+            console.log("Employee worked Full Time");
+            break;
+        default:
+            workHours = 0;
+            console.log("Employee did not work");
+            break;
+    }
+    return workHours;
+}   
 let work = Math.floor(Math.random() * 3);
-let workHours = 0;
-
-switch (work) {
-    case 1:
-        workHours = PART_TIME_HOURS;
-        console.log("Employee worked Part Time");
-        break;
-    case 2:
-        workHours = FULL_TIME_HOURS;
-        console.log("Employee worked Full Time");
-        break;
-    default:
-        workHours = 0;
-        console.log("Employee did not work");
-        break;
-}
-
-let dailyWage = workHours * WAGE_PER_HOUR;
-console.log("Daily Wage: $" + dailyWage);
+let workHours = getWorkHours(work);
+let wage = workHours * WAGE_PER_HOUR;
+console.log("Employee wage is $" + wage)
