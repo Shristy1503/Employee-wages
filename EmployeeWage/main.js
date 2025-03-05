d1ce98fa69173935bf5ca2332bdf498a84d9393d
Shristy1503 <shriimishra15@gmail.com>
2025-03-05 10:29:21 +0530

    [Shristy] Add.UC5 Functionality

--- EmployeeWage/main.js
+++ EmployeeWage/main.js
@@ -1,7 +1,9 @@
 const WAGE_PER_HOUR = 20;
 const FULL_TIME_HOURS = 8;
 const PART_TIME_HOURS = 4;
-const WORKING_DAYS_PER_MONTH = 20;
+const MAX_WORKING_DAYS = 20;
+const MAX_WORKING_HOURS = 160;
+
 
 function getWorkHours(work){
     switch (work) {
@@ -17,12 +19,24 @@ function getWorkHours(work){
     }
 }   
 let totalWage = 0;
-for (let day = 1; day <= WORKING_DAYS_PER_MONTH; day++) {
-    let workType = Math.floor(Math.random() * 3);
-    let workHours = getWorkHours(workType);
+let totalHours = 0;
+let totalDays = 0;
+
+while (totalDays < MAX_WORKING_DAYS && totalHours < MAX_WORKING_HOURS) {
+    let work = Math.floor(Math.random() * 3); 
+    let workHours = getWorkHours(work);
+    
+    if (totalHours + workHours > MAX_WORKING_HOURS) {
+        workHours = MAX_WORKING_HOURS - totalHours; 
+    }
+
     let dailyWage = workHours * WAGE_PER_HOUR;
     totalWage += dailyWage;
-    console.log(`Day ${day}: Daily Wage = $${dailyWage}`);
+    totalHours += workHours;
+    totalDays++;
+
+    console.log(`Day ${totalDays}: Worked ${workHours} hours, Daily Wage = $${dailyWage}`);
 }
 
+console.log(`Total Days Worked: ${totalDays}, Total Hours Worked: ${totalHours}`);
 console.log(`Total Monthly Wage: $${totalWage}`);
