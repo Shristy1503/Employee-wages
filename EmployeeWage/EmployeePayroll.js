cc107a445f2ace7199008e34fa0636f9e752bde0
Shristy1503 <shriimishra15@gmail.com>
2025-03-05 22:51:37 +0530

    [Shristy] Add.UC13 Functionality

--- EmployeeWage/EmployeePayroll.js
+++ EmployeeWage/EmployeePayroll.js
@@ -1,16 +1,18 @@
 
 class EmployeePayroll {
-    constructor(id, name, salary) {
+    constructor(id, name, salary, gender, startDate) {
         this.id = id;
         this.name = name;
         this.salary = salary;
+        this.gender = gender;
+        this.startDate = new Date(startDate);
     }
 
-    getDetails = () => `ID: ${this.id}, Name: ${this.name}, Salary: $${this.salary}`;
+    getDetails = () => `ID: ${this.id}, Name: ${this.name}, Salary: $${this.salary}, Gender: ${this.gender}, Start Date: ${this.startDate.toDateString()}`;
 }
 
-const emp1 = new EmployeePayroll(101, "Muskan Gupta", 50000);
-const emp2 = new EmployeePayroll(102, "Nancy Mehra", 45000);
+const emp1 = new EmployeePayroll(101, "Shristy Mishra", 50000, "Female", "2003-03-15");
+const emp2 = new EmployeePayroll(102, "Arjun Chauhan", 40000, "Male", "2003-11-05");
 
 console.log(emp1.getDetails());
 console.log(emp2.getDetails());
